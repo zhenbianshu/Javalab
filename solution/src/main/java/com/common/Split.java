7b3903334da01454684a0b6b395e8e04ba38bbc2
zhenbianshu <zhenbianshu@foxmail.com>
2019-03-25 11:21:39 +0800

    normal tests;

--- solution/src/main/java/com/common/Split.java
+++ solution/src/main/java/com/common/Split.java
@@ -7,6 +7,9 @@ public class Split {
     public static void main(String[] args) {
 
         System.out.println(String.valueOf(System.currentTimeMillis() / 1000L - 10));
+
+
+        System.out.println("xx".split("\\|").length);
     }
 }
 
---
+++ solution/src/main/java/com/common/UURL.java
@@ -0,0 +1,15 @@
+package com.common;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author zbs 2019/3/11
+ */
+public class UURL {
+    public static void main(String[] args) throws MalformedURLException {
+        URL url = new URL("xxxx");
+        System.out.println(url.getHost());
+
+    }
+}
