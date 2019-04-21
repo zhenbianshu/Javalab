1df63bce01b431838fadd1513e236548655a9b21
zhenbianshu <zhenbianshu@foxmail.com>
2019-04-21 18:36:30 +0800

    add some test;

---
+++ solution/src/main/java/com/common/GotoLabel.java
@@ -0,0 +1,16 @@
+package com.common;
+
+/**
+ * @author zbs 2019/4/18
+ */
+public class GotoLabel {
+    public static void main(String[] args) {
+        test:
+        for (int i=0;i<10;i++) {
+            System.out.println(i);
+            if (i == 5) {
+                continue test;
+            }
+        }
+    }
+}
---
+++ solution/src/main/java/com/thread/Keep.java
@@ -0,0 +1,10 @@
+package com.thread;
+
+/**
+ * @author zbs 2019/4/21
+ */
+public class Keep {
+    public static void main(String[] args) throws Exception{
+        Thread.sleep(100000);
+    }
+}
